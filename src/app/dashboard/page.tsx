9cd5a6bc8612e774d69758bf0bf5c13300e7583a
julien <julien.sarik@gmail.com>
2023-10-21 19:18:01 +0200

    add client side data fetching

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,9 @@
+import Quote from "./quote";
+
 // `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
 export default function Page() {
-    return <h1>Hello, Dashboard Page!</h1>
-  }
+    return <>
+      <h1>Hello, Dashboard Page!</h1>
+      <Quote/>
+    </>
+}
