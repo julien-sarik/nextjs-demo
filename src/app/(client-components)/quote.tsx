7e7fbee3b7f9160e3c1c545d351420c4b443a41a
julien <julien.sarik@gmail.com>
2023-10-22 13:29:24 +0200

    use environment variable

--- src/app/(client-components)/quote.tsx
+++ src/app/(client-components)/quote.tsx
@@ -6,6 +6,6 @@ import styles from './styles.module.css'
 const fetcher = (args: string) => fetch(args).then(res => res.json())
 
 export default function Quote() {
-    const { data, error, isLoading } = useSWR('https://ron-swanson-quotes.herokuapp.com/v2/quotes', fetcher)
+    const { data, error, isLoading } = useSWR(process.env.NEXT_PUBLIC_QUOTE_URL, fetcher)
     return <div className={styles.quote}>{data}</div>
 }
