285c64f70acf9e9ba087f3d239f4fdcf3016b159
julien <julien.sarik@gmail.com>
2023-10-22 13:12:57 +0200

    use CSS modules

--- src/app/(client-components)/quote.tsx
+++ src/app/(client-components)/quote.tsx
@@ -1,10 +1,11 @@
 'use client'
 
 import useSWR from 'swr'
+import styles from './styles.module.css'
 
 const fetcher = (args: string) => fetch(args).then(res => res.json())
 
 export default function Quote() {
     const { data, error, isLoading } = useSWR('https://ron-swanson-quotes.herokuapp.com/v2/quotes', fetcher)
-    return <div className='quote'>{data}</div>
+    return <div className={styles.quote}>{data}</div>
 }
--- src/app/data-fetching/layout.tsx
+++ src/app/data-fetching/layout.tsx
@@ -1,4 +1,5 @@
 import ActivableLink from "../(client-components)/activableLink"
+import styles from './styles.module.css'
 
 export default function DataFetchingLayout({
     children, // will be a page or nested layout
@@ -8,12 +9,12 @@ export default function DataFetchingLayout({
     return (
       <section>
         {/* <!-- The sidebar --> */}
-        <div className="sidebar">
+        <div className={styles.sidebar}>
           <ActivableLink href="/data-fetching/client-side" content="client component"></ActivableLink>
           <ActivableLink href="/data-fetching/server-side" content="server component"></ActivableLink>
         </div>
    
-        {children}
+        <div className={styles.content}>{children}</div>
       </section>
     )
   }
