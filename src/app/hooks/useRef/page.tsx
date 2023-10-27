d1aec98ca91c2510581d51a717e7ebbd3c938b74
julien <julien.sarik@gmail.com>
2023-10-27 21:00:21 +0200

    add comments

--- src/app/hooks/useRef/page.tsx
+++ src/app/hooks/useRef/page.tsx
@@ -2,6 +2,10 @@ import VideoPanel from "@/app/(client-components)/video-panel";
 
 export default function Page() {
     return <>
+      <div>
+        The component below uses &apos;useRef()&apos; hook to access a video HTML node.
+        It also uses &apos;useEffect()&apos; to synchronize the video player with the &apos;isPlaying&apos; state.
+      </div>
       <VideoPanel src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"/>    
     </>
 }
