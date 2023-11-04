66cf810f84099151b366eeff9f1cc7c070574c23
julien <julien.sarik@gmail.com>
2023-11-04 11:44:48 +0100

    add basic Playwright tests

--- src/app/data-fetching/page.tsx
+++ src/app/data-fetching/page.tsx
@@ -1,6 +1,7 @@
 // `app/data-fetching/page.tsx` is the UI for the `/data-fetching` URL
 export default function Page() {
     return <>
+      <h1>Data fetching</h1>
       <p>This section describes data-fetching patterns in Next.js.</p>
     </>
 }
